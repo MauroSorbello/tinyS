e36b12ce4c21e615ff8bb525683af04e67956021
nachoCoppede <nachocopede@gmail.com>
2025-03-24 20:05:07 -0300

    Modificacion scanner

--- src/main/java/analizadorLexico/Escaner.java
+++ src/main/java/analizadorLexico/Escaner.java
@@ -1,4 +1,49 @@
 package analizadorLexico;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Escaner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+
+    private int start = 0;
+    private int current = 0;
+    private int line = 0;
+
+    Escaner(String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens(){
+        while (!isAtEnd()){
+
+            //Empezamos un nuevo lexema
+            start = current;
+            scanTokens();
+        }
+
+        tokens.add(new Token(END, "", null, line));
+        return tokens;
+
+    }
+
+    private boolean isAtEnd(){
+        return current >= source.length();
+    }
+
+    private void scanToken(){
+        char c = advance();
+        switch (c){
+
+        }
+    }
+
+    private char advance(){
+        return source.charAt(current++);
+    }
+
+
+
+
 }
