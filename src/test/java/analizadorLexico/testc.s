f2f09748f5e76ce6b4926c833f32004a86d85080
MauroSorbello <sorbello.mauro.em.cn@gmail.com>
2025-05-09 18:53:48 -0300

    Escaner y lector funcionando + primeras pruebas

--- src/test/java/analizadorLexico/testc.s
+++ src/test/java/analizadorLexico/testc.s
@@ -1,49 +1,49 @@
-class Fibonacci {
-pub Int suma;
-pub Int i,j;
-}
-impl Fibonacci {
-fn Int sucesion_fib(Int n){
-i=0; j=0; suma=0;
-while (i<= n){
-if (i==0){
-(imprimo_numero(i));
-(imprimo_sucesion(suma)); }
-else
-if(i==1){
-(imprimo_numero(i));
-suma=suma+i;
-(imprimo_sucesion(suma));}
-else{
-(imprimo_numero(i));
-suma=suma+j;
-j=suma;
-(imprimo_sucesion(suma));
-}
-(++i);
-}
-ret suma;
-}
-.(){
-i=0; // inicializo i
-j=0; // inicializo j
-suma=0; // inicializo suma
-}
-fn imprimo_numero(Int num){
-(IO.out_str("f_"));
-(IO.out_int(num));
-(IO.out_str("="));
-}
-fn imprimo_sucesion(Int s){
-// El valor es:
-(IO.out_int(s));
-(IO.out_str("\n"));
-}
-}
-start{
-Fibonacci fib;
-Int n;
-fib = new Fibonacci();
-n = IO.in_int();
-(IO.out_int(fib.sucesion_fib(n)));
-}
+class Fibonacci{
+ pubIntsuma;
+ pubInti,j;
+ }
+ implFibonacci{
+ fnIntsucesion_fib(Intn){
+ i=0;j=0;suma=0;
+ while(i<=n){
+ if(i==0){
+ (imprimo_numero(i));
+ (imprimo_sucesion(suma));}
+ else
+ if(i==1){
+ (imprimo_numero(i));
+ suma=suma+i;
+ (imprimo_sucesion(suma));}
+ else{
+ (imprimo_numero(i));
+ suma=suma+j;
+ j=suma;
+ (imprimo_sucesion(suma));
+ }
+ (++i);
+ }
+ retsuma;
+ }
+ .(){
+ i=0; // inicializoi
+ j=0; // inicializoj
+ suma=0; // inicializosuma
+ }
+ fnimprimo_numero(Intnum){
+ (IO.out_str("f_"));
+ (IO.out_int(num));
+ (IO.out_str("="));
+ }
+ fnimprimo_sucesion(Ints){
+ // Elvalores:
+ (IO.out_int(s));
+ (IO.out_str("\n"));
+ }
+ }
+ start{
+ Fibonaccifib;
+ Intn;
+ fib=newFibonacci();
+ n=IO.in_int();
+ (IO.out_int(fib.sucesion_fib(n)));
+ }
