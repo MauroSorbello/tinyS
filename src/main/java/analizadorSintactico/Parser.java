926c44681451a5078b485dd52d407008a57b54dd
nachoCoppede <nachocopede@gmail.com>
2025-05-09 16:43:58 -0300

    Refactor token handling to remove unused literal field
    Removed the `literal` field and its references across classes and methods for cleaner and more efficient token management. Adjusted the `Token` class constructor and related logic, and updated test cases accordingly.

--- src/main/java/analizadorSintactico/Parser.java
+++ src/main/java/analizadorSintactico/Parser.java
@@ -1,4 +1,35 @@
 package analizadorSintactico;
 
+import analizadorLexico.ErrorLex;
+import analizadorLexico.Escaner;
+import analizadorLexico.Token;
+import analizadorLexico.TokenType;
+
+import static analizadorLexico.TokenType.CLASS;
+import static analizadorLexico.TokenType.IMPL;
+
 public class Parser {
+
+    private Token tokenActual;
+    private Escaner escaner;
+
+    //public void macheo(TokenType tokenType){
+        //if (tokenActual.getType() == tokenType){
+        //    tokenActual = escaner.
+        //}
+    //}
+
+    private void s() throws ErrorLex {
+        if (tokenActual.getType() == CLASS || tokenActual.getType()== IMPL){
+            //program();
+            //macheo(END);
+        }
+    }
+
+
+
+
+
+
+
 }
