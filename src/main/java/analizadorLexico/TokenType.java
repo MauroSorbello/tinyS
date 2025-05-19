f4ba7748e9f81759497a2f806681af981e3327a5
MauroSorbello <sorbello.mauro.em.cn@gmail.com>
2025-05-19 17:14:45 -0300

    Analisis sintactico, creacion de funcion no terminales, exepto expUn

--- src/main/java/analizadorLexico/TokenType.java
+++ src/main/java/analizadorLexico/TokenType.java
@@ -16,12 +16,12 @@ public enum TokenType {
     IMPL, ELSE, FALSE, IF, RET, WHILE, TRUE, NEW, FN, ST, PUB, SELF, DIV,CLASS,VOID,
 
     //Tokens simbolos
-    // (            )          ,     .    +      /     -      {              }            ;           [             ]           *       :
-    LEFT_PAREN, RIGHT_PAREN, COMMA, DOT, PLUS, SLASH, MINUS, LEFT_BRACE, RIGHT_BRACE, SEMICOLON, LEFT_BRACKET, RIGHT_BRACKET, MULT, DOBLE_DOT,
+    // (            )          ,     .    +      /     -      {              }            ;           [             ]           *       :           %
+    LEFT_PAREN, RIGHT_PAREN, COMMA, DOT, PLUS, SLASH, MINUS, LEFT_BRACE, RIGHT_BRACE, SEMICOLON, LEFT_BRACKET, RIGHT_BRACKET, MULT, DOBLE_DOT, PERCENTAGE,
 
     //Tokens operaciones
     // !   !=         =        ==         >           >=       <       <=        ++          --
-    NOT, NOT_EQUAL, EQUAL, EQUAL_EQUAL,GREATER,GREATER_EQUAL,LESS,LESS_EQUAL, PLUS_PLUS, MINUS_MINUS,
+    NOT, NOT_EQUAL, EQUAL, EQUAL_EQUAL,GREATER,GREATER_EQUAL,LESS,LESS_EQUAL, PLUS_PLUS, MINUS_MINUS, AND, OR,
 
     //Finalizar
     END
