6fda06829f3e71fa5521d07dcb4e36c4f77736f9
Ignacio Coppede <127450083+Coppede-Santos@users.noreply.github.com>
2025-05-23 18:52:14 -0300

    Merge pull request #39
    * Refactor error handling to use `ErrorLex` exceptions consistently.

--- src/main/java/analizadorLexico/ErrorLex.java
+++ src/main/java/analizadorLexico/ErrorLex.java
@@ -1,17 +1,8 @@
 package analizadorLexico;
 
 public class ErrorLex extends Exception{
-    public ErrorLex(){}
 
-    public ErrorLex(String msg){
-        super(msg);
-    }
-
-    public static void errorDec(int line, int column, String descripcion, String lexema){
-        report(line, column, descripcion, lexema);
-    }
-
-    private static void report(int line, int column, String descripcion, String lexema){
-        System.err.println("| LINEA " + line + "( COLUMNA: " + column + ") | " + descripcion + " " + lexema );
+    ErrorLex(int line, int column, String descripcion, String lexema){
+        super ("| LINEA " + line + "( COLUMNA: " + column + ") | " + descripcion + " " + lexema );
     }
 }
--- src/main/java/analizadorLexico/LectorCF.java
+++ src/main/java/analizadorLexico/LectorCF.java
@@ -9,22 +9,20 @@ public class LectorCF {
     private int current=0;
     private StringBuilder source= new StringBuilder();
 
-    public void lectorArchivo(String ruta) throws ErrorLex {
+    public void lectorArchivo(String ruta) throws IOException {
         try {
             this.raf = new RandomAccessFile(ruta, "r"); // Abrimos el archivo una sola vez
         }catch (IOException e){
-            ErrorLex.errorDec(0, 0, "Error al abrir el archivo", "lectorArchivo()");
-            //throw new IOException(e.getMessage());
+            throw new IOException(e.getMessage());
         }
     }
 
-    public String rechargeBuffer() throws ErrorLex, IOException {
+    public String rechargeBuffer() throws IOException {
         int caracter;
         try {
             this.raf.seek(current);
         }catch (IOException e){
-            ErrorLex.errorDec(current, 0, "Error al rellenar el buffer", "rechargeBuffer()");
-            //throw new IOException(e.getMessage());
+            throw new IOException(e.getMessage());
         }
         int i;
         for (i = current; i <= current + 2048; i++) {
